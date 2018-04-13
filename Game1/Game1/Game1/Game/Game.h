7e81b0d768b62864427f77abd1da84281233fb8e
TSITCL\s162159 <horiuchi199720@gmail.com>
2018-04-13 09:54:10 +0900

    GameMainを追加

---
+++ Game1/Game1/Game1/Game/Game.h
@@ -0,0 +1,24 @@
+﻿//
+// Game.h
+//
+
+#pragma once
+
+
+#include "MyLibrary.h"
+
+
+class Game : public MyLibrary::Framework
+{
+public:
+	Game(HINSTANCE hInstance, int nCmdShow);
+
+    void Initialize() override;
+
+	void Finalize() override;
+
+private:
+
+    void Update(MyLibrary::StepTimer const& timer);
+    void Render();
+};
